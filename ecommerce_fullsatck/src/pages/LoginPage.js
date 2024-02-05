6813aa99289cf5f1c256453443661c01c06f2306
Abhinav Panwar <panwarabhinav35@gmail.com>
2024-02-05 23:15:09 +0530

    Files added

---
+++ ecommerce_fullsatck/src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from '../features/auth/Components/Login'
+
+const LoginPage = () => {
+  return (
+    <div>
+        <Login></Login>
+    </div>
+  )
+}
+
+export default LoginPage
