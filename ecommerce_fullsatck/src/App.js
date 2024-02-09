e3d2fd267e919001dc196c5409ca8299ac195be7
Abhinav Panwar <panwarabhinav35@gmail.com>
2024-02-09 23:43:27 +0530

    Checkout added

--- ecommerce_fullsatck/src/App.js
+++ ecommerce_fullsatck/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import CartPage from './pages/CartPage';
+import Checkout from './pages/Checkout';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
@@ -28,6 +29,10 @@ const router = createBrowserRouter([
   {
     path: "/cart",
     element: <CartPage></CartPage>
+  },
+  {
+    path: "/checkout",
+    element: <Checkout></Checkout>
   },
 ]);
 
